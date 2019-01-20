cbd1f443d62cf8ad57b4233503ebcbd8abbf9c5a
mbreiter <matthew.reiter@mail.utoronto.ca>
2019-01-20 06:46:12 +0000

    adding gurobi

---
+++ env/bin/gurobi810/linux64/src/cpp/Expr.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2018, Gurobi Optimization, LLC
+// All Rights Reserved
+#ifndef _EXPR_CPP_H_
+#define _EXPR_CPP_H_
+
+class GRBExpr
+{
+  private:
+
+  public:
+    friend class GRBModel;
+};
+#endif
